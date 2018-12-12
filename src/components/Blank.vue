0cf072904f678d4c17a37f663657d00bacd74c23
Jaime Lopez <loama18@gmail.com>
2018-12-12 18:42:37 -0300

    init firebase
    this commit is also sending changes to files like app.vue and blank.vue bc those were not added to git at the beginning

---
+++ src/components/Blank.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blank">
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blank',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+</style>
