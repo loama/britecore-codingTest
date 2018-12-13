9172c5ee8173002cdb63d285de899c4d91dc5a77
Jaime Lopez <loama18@gmail.com>
2018-12-12 21:06:53 -0300

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="navbar">
+      <img src="./assets/logo.png" class="logo">
+    </div>
+
     <Blank msg="This is the wowow branch"/>
   </div>
 </template>
@@ -16,11 +20,29 @@ export default {
 </script>
 
 <style lang="sass">
+  body, html
+    margin: 0
+    padding: 0
+    background: #FBFBFD
+
   #app
     font-family: 'Avenir', Helvetica, Arial, sans-serif
     -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing: grayscale
-    text-align: center
+    text-align: left
     color: #2c3e50
-    margin-top: 60px
+
+  #navbar
+    position: fixed
+    top: 0
+    left: 0
+    height: 56px
+    width: 100vw
+    background: #FFFFFF
+    border-bottom: 1px solid #E0E0E0
+
+    .logo
+      height: 20px
+      margin-top: 18px
+      margin-left: 16px
 </style>
